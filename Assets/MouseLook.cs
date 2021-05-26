8fd5799dc5a1c420e9e07f1c8e86ad879b9688d0
Batgame <batgen42@gmail.com>
2021-05-26 09:32:52 +0200

    Another One

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -21,7 +21,8 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        
+
+        Debug.Log(mouseY);
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
