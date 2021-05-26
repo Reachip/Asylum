e98d4fd60460d9eb72f507e04b9e0c5031576525
jo15122002 <lapinette2002@ymail.com>
2021-05-26 11:23:08 +0200

    Clignotement du videoproj

---
+++ Assets/Scripts/DayRoom/lightBeamVideoProjClignotement.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightBeamVideoProjClignotement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Light lightBeam;
+    System.Random rnd = new System.Random();
+
+    void Start()
+    {
+        lightBeam = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightBeam.intensity = rnd.Next(1,9);
+    }
+}
