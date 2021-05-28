b181f4f8c96809eb16ceda8c047540dbc027a666
Bastien GENOD <genodb@iut-acy.local>
2021-05-28 12:00:00 +0200

    Hello World

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +23,30 @@ public class GameOver : MonoBehaviour
 
     public void Menu()
     {
-        SceneManager.LoadScene("Show");
+        StartCoroutine(LoadAsync(1));
+        
     }
 
     public void Quitter()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            //float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = operation.progress;
+            progressText.text = operation.progress * 100f + "%";
+            yield return null;
+        }
+
     }
 }
